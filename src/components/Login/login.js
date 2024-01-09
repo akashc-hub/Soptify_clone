5716c652972fd93645ba48c6910adb820e3fe893
Abhishek Nikam <79035081+Abhisheknik@users.noreply.github.com>
2024-01-09 11:04:15 +0530

    Update Mesage

---
+++ src/components/Login/login.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./login.css";
+
+
+
+
+function Login() {
+  return (
+    <div className="login">
+      {/* <img
+        // src=""
+        // alt="Spotify logo"
+      /> 
+     <a href="#">LOGIN WITH SPOTIFY</a> */}
+    </div>
+  );
+}
+
+export default Login;
